bc2551224acf9391e159802ff04e3a6f905e8105
Matheus Borges <matob@live.com>
2024-09-11 20:58:26 -0300

    fix: fix mongo connection

--- src/config/mongo/MongoConfig.ts
+++ src/config/mongo/MongoConfig.ts
@@ -1,4 +1,9 @@
 
 import { Environment as envs } from '@/infra/web/nestjs/environment'
 
-export default `mongodb${envs.MONGO_PORT ? '' : '+srv'}://${envs.MONGO_USERNAME}:${envs.MONGO_PASSWORD}@${envs.MONGO_HOSTNAME}${envs.MONGO_PORT ? ':' + envs.MONGO_PORT : ''}/${envs.MONGO_DATABASE}?authSource=admin&directConnection=true`
+const protocol = `mongodb${envs.MONGO_PORT ? '' : '+srv'}`
+const auth = `${envs.MONGO_USERNAME}:${envs.MONGO_PASSWORD}`
+const host = `${envs.MONGO_HOSTNAME}${envs.MONGO_PORT ? ':' + envs.MONGO_PORT : ''}`
+const queryParams = `authSource=admin&directConnection=${envs.MONGO_PORT ? 'true' : 'false'}`
+
+export default `${protocol}://${auth}@${host}/${envs.MONGO_DATABASE}?${queryParams}`
